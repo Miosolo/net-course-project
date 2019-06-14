4f7d534b960676c2d849deb4cd32e610772e1d4d
miosolo <xht2012.jn@gmail.com>
2019-06-14 12:56:10 +0800

    fixed bug of UDP chatting

--- udp-chat/client.c
+++ udp-chat/client.c
@@ -7,14 +7,14 @@
 #include <unistd.h>
 
 #define FAIL -1 // define a friendly flag
-#define BUF_SIZE 1 << 10
+#define BUF_SIZE 1 << 10 // client I/O buffer size
 
 // Plz add the -C99 flag to compile
 int main(int argc, char *argv[]) {
   char *server_addr = argv[1]; // server address
-  int server_port; // server port
-  if (argc < 2 || sscanf(argv[2], "%d", &server_port) != 1) {// check the input params' amount and format
-    printf("command line params should be (address, port)\n");
+  short unsigned int server_port; // server port, 16 bits
+  if (argc < 3 || sscanf(argv[2], "%i", &server_port) != 1) {// check the input params' amount and format
+    printf("command line params should be (IP, port)\n");
     return 1; // abnormal exit
   }
 
@@ -29,28 +29,29 @@ int main(int argc, char *argv[]) {
   // init server addr
   struct sockaddr_in remote_addr = {0}; // set to 0
   // memset(&remote_addr, 0, sizeof(remote_addr));
-	remote_addr.sin_family = AF_INET; // UDP
+	remote_addr.sin_family = AF_INET; // IPv4
 	remote_addr.sin_addr.s_addr = inet_addr(server_addr); // server IP
 	remote_addr.sin_port = htons(server_port); // server port
   
   char buffer[BUF_SIZE];  // the buffer used as pipe of stdin <=> client <=> server
   // starting connection loop
   while(scanf("%s", buffer)) { // scanf returns the valid param amount, so scanf()==1 <=> valid input
-    if (strcmp(buffer, "exit") == 0) { // input "exit"
-      printf("bye\n");
-      return 0;
-    }
-
     long len = 0; // the length of success transfer
-	  if (len = sendto(client_sockfd, buffer, strlen(buffer), 0,
-      (struct sockaddr *)&remote_addr, sizeof(struct sockaddr)) == FAIL) { // send data to the address & port specified
+	  if ((len = sendto(client_sockfd, buffer, strlen(buffer), 0,
+      (struct sockaddr *)&remote_addr, sizeof(struct sockaddr))) == FAIL) { // send data to the address & port specified
         perror("sending data to server");
         return 1;
       }
     
-    int addr_len = sizeof(struct sockaddr_in);
-    if (len = recvfrom(client_sockfd, buffer, BUF_SIZE-1, 0, 
-      (struct sockaddr *)&remote_addr, (socklen_t *)&addr_len) == FAIL) { // receive data from the address & port specified
+    if (strncmp(buffer, "exit", BUF_SIZE) == 0) {
+      // if inputed "exit": send to server, then close it self
+      printf("bye\n");
+      return 0;
+    }
+    
+    unsigned int addr_len = sizeof(struct sockaddr_in);
+    if ((len = recvfrom(client_sockfd, buffer, BUF_SIZE-1, 0, 
+      (struct sockaddr *)&remote_addr, (socklen_t *)&addr_len)) == FAIL) { // receive data from the address & port specified
         perror("receiving from server"); 
         return 1;
       }
